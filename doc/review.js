6eff905efb3cf6f87473a145a75dea8e3e2f125c
someone <someone@universe.all>
2025-02-17 14:15:05 +0100

    multiple review groups implemented

--- doc/review.js
+++ doc/review.js
@@ -1,33 +1,47 @@
-var reviewIsOn = false
-var hiddenReviewClass = "hidden_review"
-var reviewIsOnClass = "review_is_on"
-function toggleReview() {
-    let reviewedElements = document.getElementsByClassName("rv")
-    if (reviewIsOn) {
+const reviewStates = [
+    {isOn: false, reviewItemClass: "rv", reviewTogglerId: "reviewToggler",
+        hiddenReviewClass: "hidden_review",
+        showContentFunction: createShowReviewedContentFunction("hidden_review")}, 
+    {isOn: false, reviewItemClass: "rv1", reviewTogglerId: "reviewToggler1",
+        hiddenReviewClass: "hidden_review1",
+        showContentFunction: createShowReviewedContentFunction("hidden_review1")}
+]
+
+const reviewIsOnClass = "review_is_on"
+const reviewIsHiddenClass = "reviewHidden"
+
+function createShowReviewedContentFunction(hiddenReviewClass) {
+    return function () {
+        if (this.classList.contains(hiddenReviewClass)) {
+            this.classList.remove(hiddenReviewClass)
+        } else {
+            this.classList.add(hiddenReviewClass)
+        }
+        return false;
+    }
+}
+
+function toggleReview(reviewStateIndex) {
+    let reviewState = reviewStates[reviewStateIndex]
+    let reviewedElements = document.getElementsByClassName(reviewState.reviewItemClass)
+    
+    if (reviewState.isOn) {
         for (let el of reviewedElements) {
-            el.classList.remove(hiddenReviewClass)
-            el.removeEventListener("click", showRevewedContent)
+            el.classList.remove(reviewState.hiddenReviewClass)
+            el.removeEventListener("click", reviewState.showContentFunction)
             el.classList.remove(reviewIsOnClass)
         }
-        document.getElementById("reviewToggler").classList.remove("reviewHidden")
-        reviewIsOn = false
+        document.getElementById(reviewState.reviewTogglerId).classList.remove(reviewIsHiddenClass)
+        reviewState.isOn = false
     } else {
         for (let el of reviewedElements) {
-            el.classList.add(hiddenReviewClass)
-            el.addEventListener("click", showRevewedContent)
+            el.classList.add(reviewState.hiddenReviewClass)
+            el.addEventListener("click", reviewState.showContentFunction)
             el.classList.add(reviewIsOnClass)
         }
-        document.getElementById("reviewToggler").classList.add("reviewHidden")
-        reviewIsOn = true
+        document.getElementById(reviewState.reviewTogglerId).classList.add(reviewIsHiddenClass)
+        reviewState.isOn = true
     }
     return false
 }
 
-function showRevewedContent() {
-    if (this.classList.contains(hiddenReviewClass)) {
-        this.classList.remove(hiddenReviewClass)
-    } else {
-        this.classList.add(hiddenReviewClass)
-    }
-    return false
-}
